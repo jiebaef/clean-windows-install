bf0c5576fff51cd6f6fbf9deb699d0df6f16d494
jiebaef <github.sfzfba@jiebaef.dev>
2023-10-24 14:16:34 +0200

    added disabling of new windows feeds

---
+++ clean-windows-install.ps1
@@ -0,0 +1,19 @@
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds"
+$name = "EnableFeeds"
+$value = "0"
+ 
+$registryPath2 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"
+$name2 = "ShellFeedsTaskbarViewMode"
+$value2 = "2"
+ 
+if ( -Not ( Test-Path $registryPath ) ) {
+    New-Item -Path $registryPath -ItemType RegistryKey -Force
+}
+ 
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+ 
+if ( -Not ( Test-Path $registryPath2 ) ) {
+    New-Item -Path $registryPath2 -ItemType RegistryKey -Force
+}
+ 
+New-ItemProperty -Path $registryPath2 -Name $name2 -Value $value2 -PropertyType DWORD -Force | Out-Null
